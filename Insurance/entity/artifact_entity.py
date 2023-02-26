3a8b6d670733f5b963ad83ad8d8523cd015bfd17
kanishkakum <kanishkakumar408@gmail.com>
2023-02-27 00:38:13 +0530

    created pipeline basic model, exception and logger file

---
+++ Insurance/entity/artifact_entity.py
@@ -0,0 +1,17 @@
+class DataIngestion:
+    pass
+
+class DataValidation:
+    pass
+
+class DataTransformtion:
+    pass
+
+class ModelTrainer:
+    pass
+
+class ModelEvaluation:
+    pass
+
+class ModelPusher:
+    pass
