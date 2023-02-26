61c15c89877cad9e74cb53ee80f59b67ed9beb13
kanishkakum <kanishkakumar408@gmail.com>
2023-02-27 00:53:17 +0530

    data Ingestion .......

--- Insurance/entity/artifact_entity.py
+++ Insurance/entity/artifact_entity.py
@@ -1,5 +1,10 @@
-class DataIngestion:
-    pass
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_file_path:str
+    train_file_path:str 
+    test_file_path:str
 
 class DataValidation:
     pass
